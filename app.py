0bd3bfe8c38635c5a72c4909b7d825bbe64e4725
C-o-m-o-n <comon928@gmail.com>
2024-01-05 15:26:34 +0300

    Add authentication

--- app.py
+++ app.py
@@ -5,15 +5,67 @@ from geminiGenerate.generate import generateOutput
 import random
 import time
 
+SERVICE_ACCOUNT_KEY_PATH = st.secrets["SERVICE_ACCOUNT_KEY_PATH"]
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate("path/to/serviceAccountKey.json")  # Replace with your service account key path
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    cred = credentials.Certificate(SERVICE_ACCOUNT_KEY_PATH)
+    firebase_admin.initialize_app(cred)
 
+# Initialize session_state
+if "user" not in st.session_state:
+    st.session_state.user = None
 
 st.set_page_config(
             page_title="OutBard",
             page_icon="ocean",
         )
+
+def authentication_page():
+    st.title("Login or signup to continue")
+    # st.sidebar.title("OutBardAuthentication")
+    
+    email = st.text_input("Enter your email:")
+    password = st.text_input("Enter your password:", type="password")
+
+    col1, col2 = st.columns(2,gap="small")
+
+    with col1:
+        login_button = st.button("Login", help="Login to your account",)
+
+    with col2:
+        signup_button = st.button("Signup", help="Create an account",type="primary",)
+
+    if signup_button:
+        try:
+            user = auth.create_user(email=email, password=password)
+            st.success(f"User created successfully: {user.email}")
+            st.session_state.user = user
+            # st.success("Signup successful!")
+
+            st.empty()
+            with st.spinner("Please wait..."):
+                time.sleep(2)
+                st.experimental_rerun()
+            main_app()
+
+        except Exception as e:
+            st.error(f"Signup failed: {e}")
+
+    if login_button:
+        try:
+            user = auth.get_user_by_email(email)
+            st.session_state.user = user
+            st.success(f"Login successfuly as {user.email}")
+
+            st.empty()
+            with st.spinner("Please wait..."):
+                time.sleep(2)
+                st.experimental_rerun()
+            main_app()
+
+        except Exception as e:
+            st.error(f"Login failed: {e}")
+            
 def main_app():
 
     st.title("Welcome to OutBard")
@@ -26,7 +78,7 @@ def main_app():
     full_response = ""
 
     # Accept user input
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("What's up?"):
         # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(prompt)
@@ -51,15 +103,7 @@ def main_app():
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
-def authentication_page():
-    st.title("Welcome to OutBard Authentication page")
-    st.sidebar.title("OutBardAuthentication")
-
-# Create a simple navigation menu
-pages = ["Home", "Authentication"]
-selected_page = st.sidebar.selectbox("Select Page", pages)
-# Define pages
-if selected_page == "Home":
+if st.session_state.user:
     main_app()
-elif selected_page == "Authentication":
+else:
     authentication_page()
